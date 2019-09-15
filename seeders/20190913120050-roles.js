eb483a9f38771eb418004c5d51fcff6aa69fe966
William <binet.william.33@gmail.com>
2019-09-15 21:21:04 +0200

    Load basic data in db

--- seeders/20190913120050-roles.js
+++ seeders/20190913120050-roles.js
@@ -1,33 +1,39 @@
 'use strict';
+var modele = require('../models/index');
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Roles', [
-      {
-        label: 'Administrator',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        label: 'Manager',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        label: 'Employee',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ], {});
-  },
+    up: (queryInterface, Sequelize) => {
+        let role = modele.Role.findAll();
+        if (role.length !== 0) {
+            return queryInterface.bulkInsert('Roles', [
+                {
+                    label: 'Administrator',
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    label: 'Manager',
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    label: 'Employee',
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                }
+            ], {});
+        } else {
+          return queryInterface.bulkInsert('Roles', []);
+        }
+    },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+    down: (queryInterface, Sequelize) => {
+        /*
+          Add reverting commands here.
+          Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
+          Example:
+          return queryInterface.bulkDelete('People', null, {});
+        */
+    }
 };
--- seeders/20190915175528-Users.js
+++ seeders/20190915175528-Users.js
@@ -1,58 +1,63 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [
-      {
-        email: 'Administrator@email.fr',
-        username : 'Administrator',
-        password : '$2y$12$ZIVXrHduOT.oAAsmF5i7IOOP0s0Pq3aJ5Do76t..wA3ZViih5H9GG',
-        firstname : 'fname',
-        lastname : 'lname',
-        id_role : '1',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        email: 'Manager@email.fr',
-        username : 'Manager',
-        password : '$2y$12$ZIVXrHduOT.oAAsmF5i7IOOP0s0Pq3aJ5Do76t..wA3ZViih5H9GG',
-        firstname : 'fname',
-        lastname : 'lname',
-        id_role : '2',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        email: 'Employee1@email.fr',
-        username : 'Employee1',
-        password : '$2y$12$ZIVXrHduOT.oAAsmF5i7IOOP0s0Pq3aJ5Do76t..wA3ZViih5H9GG',
-        firstname : 'fname',
-        lastname : 'lname',
-        id_role : '3',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        email: 'Employee2@email.fr',
-        username : 'Employee2',
-        password : '$2y$12$ZIVXrHduOT.oAAsmF5i7IOOP0s0Pq3aJ5Do76t..wA3ZViih5H9GG',
-        firstname : 'fname',
-        lastname : 'lname',
-        id_role : '3',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ], {});
-  },
+    up: (queryInterface, Sequelize) => {
+        let users = modele.User.findAll();
+        if (users.length !== 0) {
+            return queryInterface.bulkInsert('Users', [
+                {
+                    email: 'Administrator@email.fr',
+                    username: 'Administrator',
+                    password: '$2y$12$ZIVXrHduOT.oAAsmF5i7IOOP0s0Pq3aJ5Do76t..wA3ZViih5H9GG',
+                    firstname: 'fname',
+                    lastname: 'lname',
+                    id_role: '1',
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    email: 'Manager@email.fr',
+                    username: 'Manager',
+                    password: '$2y$12$ZIVXrHduOT.oAAsmF5i7IOOP0s0Pq3aJ5Do76t..wA3ZViih5H9GG',
+                    firstname: 'fname',
+                    lastname: 'lname',
+                    id_role: '2',
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    email: 'Employee1@email.fr',
+                    username: 'Employee1',
+                    password: '$2y$12$ZIVXrHduOT.oAAsmF5i7IOOP0s0Pq3aJ5Do76t..wA3ZViih5H9GG',
+                    firstname: 'fname',
+                    lastname: 'lname',
+                    id_role: '3',
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    email: 'Employee2@email.fr',
+                    username: 'Employee2',
+                    password: '$2y$12$ZIVXrHduOT.oAAsmF5i7IOOP0s0Pq3aJ5Do76t..wA3ZViih5H9GG',
+                    firstname: 'fname',
+                    lastname: 'lname',
+                    id_role: '3',
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                }
+            ], {});
+        } else {
+            return queryInterface.bulkInsert('Users', []);
+        }
+    },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+    down: (queryInterface, Sequelize) => {
+        /*
+          Add reverting commands here.
+          Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
+          Example:
+          return queryInterface.bulkDelete('People', null, {});
+        */
+    }
 };
